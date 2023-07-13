071af8ce8cd5ce98d4b48cce30a9fe930e7dfb2f
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2023-07-13 21:39:12 +0500

    include support for .6 file document

--- setup.py
+++ setup.py
@@ -7,11 +7,14 @@ setup(
     author_email='your_email@example.com',
     description='Description of your tool',
     long_description='Long description of your tool',
-    url='https://github.com/AitzAmm/mytool',
+    url='https://github.com/yourusername/mytool',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
             'mytool = src.mytool:main',
         ],
     },
+    data_files=[
+        ('share/man/man6', ['doc/mytool.6']),
+    ],
 )
